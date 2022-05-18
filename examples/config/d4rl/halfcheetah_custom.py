1669012cfea95d801d5871cfd6aa10e591d2f7e5
Alan Clark <32013626+thatscotdatasci@users.noreply.github.com>
2022-05-18 11:04:23 +0100

    Custom config

--- examples/config/d4rl/halfcheetah_custom.py
+++ examples/config/d4rl/halfcheetah_custom.py
@@ -4,10 +4,10 @@ params = deepcopy(mopo_params)
 params.update({
     'domain': 'HalfCheetah',
     'task': 'v2',
-    'exp_name': 'halfcheetah_dogo'
+    'exp_name': 'halfcheetah_mixed_capabilities'
 })
 params['kwargs'].update({
-    'pool_load_path': '/home/ajc348/rds/hpc-work/mopo/rollouts/softlearning/HalfCheetah/v3/2022-05-16T12-29-56-my-sac-experiment-1/id=8592b_00000-seed=4378/combined_transitions.npy',
+    'pool_load_path': '/home/ajc348/rds/hpc-work/mopo/rollouts/softlearning/HalfCheetah/v2/2022-05-17T18-38-36-half_cheetah_v2_3M/id=31acc_00000-seed=9479/combined_transitions.npy',
     'pool_load_max_size': 101000,
     'rollout_length': 5,
     'penalty_coeff': 1.0
