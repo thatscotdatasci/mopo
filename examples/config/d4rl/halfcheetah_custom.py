8b607f232d9a0e6d30d366211dd8a3bbbeb2b4a0
Alan Clark <32013626+thatscotdatasci@users.noreply.github.com>
2022-06-08 10:16:11 +0100

    Minor tweaks to config

--- examples/config/d4rl/halfcheetah_custom.py
+++ examples/config/d4rl/halfcheetah_custom.py
@@ -4,11 +4,11 @@ params = deepcopy(mopo_params)
 params.update({
     'domain': 'HalfCheetah',
     'task': 'v2',
-    'exp_name': 'halfcheetah_d3rlpy_pap5',
+    'exp_name': 'halfcheetah_d3rlpy_mp1',
     'seed': 4321,
 })
 params['kwargs'].update({
-    'pool_load_path': '/home/ajc348/rds/hpc-work/dogo_results/data/D3RLPY-PAP5-P1-4.npy',
+    'pool_load_path': '/home/ajc348/rds/hpc-work/dogo_results/data/D3RLPY-MP1-P1-4.npy',
     # 'model_load_dir': '/home/ajc348/rds/hpc-work/dogo_results/mopo/ray_mopo/HalfCheetah/halfcheetah_d3rlpy_pep4_101e3/seed:1443_2022-06-01_17-07-30wh6i6w19/models',
     'pool_load_max_size': 101000,
     'rollout_length': 5,
