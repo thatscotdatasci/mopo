a881e842f489c2b5dbf312d54f457445c7e21427
Alan Clark <32013626+thatscotdatasci@users.noreply.github.com>
2022-07-10 12:52:04 +0100

    Save D4RL datasets

---
+++ d4rl_datasets.py
@@ -0,0 +1,16 @@
+import gym
+import d4rl
+import numpy as np
+
+name = 'halfcheetah-random-v0'
+env = gym.make(name)
+data = d4rl.qlearning_dataset(env)
+data_arr = np.hstack([
+    data['observations'],
+    data['actions'],
+    data['next_observations'],
+    data['rewards'][:,None],
+    data['terminals'][:,None],
+    np.zeros_like(data['rewards'])[:,None]
+])
+# np.save('../dogo_results/data/D4RL-HC-R.npy', data_arr)
