8c32e36f78f489b995da254bc8f4876ea20f051b
Alan Clark <32013626+thatscotdatasci@users.noreply.github.com>
2022-07-12 13:47:09 +0100

    Updates and tidying of plotting scripts

--- dogo/visualisation/dynamics_analysis_plotting.py
+++ dogo/visualisation/dynamics_analysis_plotting.py
@@ -5,6 +5,8 @@ import matplotlib.pyplot as plt
 
 from dogo.results import get_experiment_details
 
+cols = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf']
+
 def retrieve_metric(metric, exp, dataset):
     return np.load(os.path.join('dynamics', f'{exp}_{dataset}_{metric}.npy'), allow_pickle=True)
 
@@ -12,11 +14,14 @@ def plot_metric_2d(experiment_names, metric, dataset):
     fig, ax = plt.subplots(1, 1, figsize=(10,10))
     x_vals = np.load(f'/home/ajc348/rds/hpc-work/dogo_results/data/pca/{dataset}_1d.npy')[:10000,:]
 
-    for exp_name in experiment_names:
+    for i, exp_name in enumerate(experiment_names):
         exp = get_experiment_details(exp_name)
         vals_arr = retrieve_metric(metric, exp.name, dataset)
         y_vals = vals_arr.mean(axis=1)
-        ax.scatter(x_vals, y_vals, marker='x', label=f'{exp.name} - REx: {exp.rex} - Beta: {exp.rex_beta} - Seed: {exp.seed}')
+        ax.scatter(x_vals, y_vals, marker='x', color=cols[i], label=f'{exp.name} - REx: {exp.rex} - Beta: {exp.rex_beta} - Seed: {exp.seed}')
+
+        if metric == 'reward_pens':
+            ax.axhline(exp.max_penalty, ls='--', color=cols[i])
 
     ax.legend()
 
--- dogo/visualisation/policy_dynamics_plotting.py
+++ dogo/visualisation/policy_dynamics_plotting.py
@@ -3,6 +3,8 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 
+from dogo.results import get_experiment_details
+
 cols = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf']
 
 def retrieve_metric(dynamics_exp, policy_exp, deterministic_model, deterministic_policy, env, metric):
@@ -17,6 +19,23 @@ def retrieve_metric_stats(dynamics_exp, policy_exp, deterministic_model, determi
         'std': arr.std(axis=-1).flatten()
     }
 
+def policy_dynamics_plot_title(dynamics_exp, policy_exp):
+    dynamics_exp_details = get_experiment_details(dynamics_exp)
+    policy_exp_details = get_experiment_details(policy_exp)
+    policy_dynamics_exp_details = get_experiment_details(policy_exp_details.dynamics_model_exp)
+
+    if dynamics_exp_details.rex:
+        dynamics_title = f'Dynamics: {dynamics_exp_details.name} - REx: True - REx Beta: {dynamics_exp_details.rex_beta} - Seed: {dynamics_exp_details.seed}'
+    else:
+        dynamics_title = f'Dynamics: {dynamics_exp_details.name} - REx: False - Seed: {dynamics_exp_details.seed}'
+
+    if policy_dynamics_exp_details.rex:
+        policy_title = f'Policy: {policy_exp_details.name} - REx: True - REx Beta: {policy_dynamics_exp_details.rex_beta} - Seed: {policy_dynamics_exp_details.seed}'
+    else:
+        policy_title = f'Policy: {policy_exp_details.name} - REx: False - Seed: {policy_dynamics_exp_details.seed}'
+    
+    return f'{dynamics_title}\n{policy_title}'
+
 def plot_cumulative_reward(dynamics_exp, policy_exp):
     fig, ax = plt.subplots(1, 1, figsize=(20,10))
 
@@ -28,10 +47,10 @@ def plot_cumulative_reward(dynamics_exp, policy_exp):
     n_records = len(fake_unpen_rewards)
 
     for i, (metric, label) in enumerate([
-        (fake_unpen_rewards, 'Model'),
-        (fake_pen, 'Penalty'),
-        (eval_rewards, 'Eval'),
-        (gym_rewards, 'Gym'),
+        (fake_unpen_rewards, 'Learned Reward - No Penalty'),
+        (eval_rewards, 'True Reward - No Penalty'),
+        (fake_pen, 'MOPO Penalty'),
+        (gym_rewards, 'Gym Rollout'),
     ]):
         cumsum_arr = metric.cumsum(axis=0)
         mean_arr = cumsum_arr.mean(axis=-1).flatten()
@@ -39,6 +58,8 @@ def plot_cumulative_reward(dynamics_exp, policy_exp):
         max_arr = cumsum_arr.max(axis=-1).flatten()
         ax.plot(mean_arr, c=cols[i], label=label)
         ax.fill_between(np.arange(n_records), min_arr, max_arr, color=cols[i], alpha=0.5)
+
+    ax.set_title(policy_dynamics_plot_title(dynamics_exp, policy_exp))
 
     ax.legend()
 
@@ -63,9 +84,9 @@ def plot_reward_landscape(dynamics_exp, policy_exp):
     gym_rewards_arrs = retrieve_metric(dynamics_exp, policy_exp, True, True, 'gym', 'rewards').transpose(2,0,1).flatten()
     
     for i in range(2):
-        ax[i].scatter(fake_pca_2d_arrs[:,0], fake_pca_2d_arrs[:,1], fake_unpen_rewards_arrs, marker='x', s=10, label='Model')
-        ax[i].scatter(fake_pca_2d_arrs[:,0], fake_pca_2d_arrs[:,1], eval_rewards_arrs, marker='x', s=10, label='Eval')
-        ax[i].scatter(gym_pca_2d_arrs[:,0], gym_pca_2d_arrs[:,1], gym_rewards_arrs, marker='x', s=10, label='Gym')
+        ax[i].scatter(fake_pca_2d_arrs[:,0], fake_pca_2d_arrs[:,1], fake_unpen_rewards_arrs, marker='x', s=10, label='Learned Reward')
+        ax[i].scatter(fake_pca_2d_arrs[:,0], fake_pca_2d_arrs[:,1], eval_rewards_arrs, marker='x', s=10, label='True Reward')
+        ax[i].scatter(gym_pca_2d_arrs[:,0], gym_pca_2d_arrs[:,1], gym_rewards_arrs, marker='x', s=10, label='Gym Rollout')
     
     ax[0].view_init(10, 60)
     ax[1].view_init(90, 0)
@@ -75,6 +96,8 @@ def plot_reward_landscape(dynamics_exp, policy_exp):
         ax[i].set_ylabel('PCA Dimension 2')
         ax[i].set_zlabel('Reward')
         ax[i].legend()
+
+    fig.suptitle(policy_dynamics_plot_title(dynamics_exp, policy_exp))
 
 def plot_metric_landscape_comp(dynamics_policy_exps_list, metric):
     fig, ax = plt.subplots(1, 2, figsize=(20,10), subplot_kw={"projection": "3d"})
@@ -94,6 +117,59 @@ def plot_metric_landscape_comp(dynamics_policy_exps_list, metric):
         ax[i].set_ylabel('PCA Dimension 2')
         ax[i].set_zlabel((' '.join(metric.split('_'))).title())
         ax[i].legend()
+
+def plot_returns_comparison(dynamics_exps, policy_exps):
+    fig, ax = plt.subplots(1, 3, figsize=(30,10))
+    
+    dynamics_exps_labels = []
+    policy_exps_labels = []
+    fake_returns = np.zeros((len(dynamics_exps), len(policy_exps), 2))
+    eval_returns = np.zeros((len(dynamics_exps), len(policy_exps), 2))
+    gym_returns = np.zeros((len(dynamics_exps), len(policy_exps), 2))
+    for i, dynamics_exp in enumerate(dynamics_exps):
+        dynamics_exp_details = get_experiment_details(dynamics_exp)
+        if dynamics_exp_details.rex:
+            dynamics_exps_labels.append(f'{dynamics_exp} - REx: {dynamics_exp_details.rex}\nREx Beta: {dynamics_exp_details.rex_beta}\nSeed: {dynamics_exp_details.seed}')
+        else:
+            dynamics_exps_labels.append(f'{dynamics_exp} - REx: {dynamics_exp_details.rex}')
+        
+        for j, policy_exp in enumerate(policy_exps):
+            if i == 0:
+                policy_exp_details = get_experiment_details(policy_exp)
+                policy_dynamics_exp_details = get_experiment_details(policy_exp_details.dynamics_model_exp)
+                if policy_dynamics_exp_details.rex:
+                    policy_exps_labels.append(f'{policy_dynamics_exp_details.name} - REx: {policy_dynamics_exp_details.rex}\nREx Beta: {policy_dynamics_exp_details.rex_beta}\nSeed: {dynamics_exp_details.seed}')
+                else:
+                    policy_exps_labels.append(f'{policy_dynamics_exp_details.name} - REx: {policy_dynamics_exp_details.rex}\nSeed: {dynamics_exp_details.seed}')
+
+            fake_metric_arrs = retrieve_metric(dynamics_exp, policy_exp, True, True, 'fake', 'unpen_rewards').cumsum(axis=0)[-1,0,:]
+            fake_returns[i,j,0], fake_returns[i,j,1] = fake_metric_arrs.mean(), fake_metric_arrs.std()
+
+            eval_metric_arrs = retrieve_metric(dynamics_exp, policy_exp, True, True, 'eval', 'rewards').cumsum(axis=0)[-1,0,:]
+            eval_returns[i,j,0], eval_returns[i,j,1] = eval_metric_arrs.mean(), eval_metric_arrs.std()
+
+            gym_metric_arrs = retrieve_metric(dynamics_exp, policy_exp, True, True, 'gym', 'rewards').cumsum(axis=0)[-1,0,:]
+            gym_returns[i,j,0], gym_returns[i,j,1] = gym_metric_arrs.mean(), gym_metric_arrs.std()
+    
+    for i, (res_arr, title) in enumerate([
+        (fake_returns, 'Learned Dynamics'),
+        (eval_returns, 'True Dynamics'),
+        (gym_returns, 'Gym Rollout')
+    ]):
+        mat = ax[i].matshow(res_arr[:,:,0], cmap='viridis')
+        ax[i].set_xticks(range(len(policy_exps)))
+        ax[i].set_yticks(range(len(dynamics_exps)))
+        ax[i].set_xticklabels(policy_exps_labels, rotation=45)
+        ax[i].set_yticklabels(dynamics_exps_labels, rotation=45)
+        ax[i].set_title(title)
+
+        for (j,k), z in np.ndenumerate(res_arr[:,:,0]):
+            if z != 0:
+                ax[i].text(k, j, '{:.2f}\n±{:.2f}'.format(res_arr[j,k,0], res_arr[j,k,1]), ha="center", va="center", color='w' if z < 1000 else 'k')
+
+    for i in range(3):
+        ax[i].set_xlabel('Policy')
+        ax[i].set_ylabel('Dynamics')
 
 def plot_pen_reward_landscape_comp(dynamics_policy_exps_list, pen_coeff=1.0):
     fig, ax = plt.subplots(1, 2, figsize=(20,10), subplot_kw={"projection": "3d"})
