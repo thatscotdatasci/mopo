62cd5274167c62f3c2b6ced43915200fa4dbe6c5
Alan Clark <32013626+thatscotdatasci@users.noreply.github.com>
2022-07-14 13:30:35 +0100

    Migrate rollout splitting function from dogo_results

---
+++ dogo/rollouts/split.py
@@ -0,0 +1,23 @@
+from collections import namedtuple
+
+import numpy as np
+
+#################################################
+# Dimensions for HalfCheetah v2 dataset
+# DO NOT TRY TO APPLY TO OTHERS WITHOUT CHECKING!
+#################################################
+STATE_DIMS = 17
+ACTION_DIMS = 6
+REWARD_DIMS = 1
+TERMINAL_DIMS = 1
+POLICY_DIMS = 1
+
+DIMS = [STATE_DIMS, ACTION_DIMS, STATE_DIMS, REWARD_DIMS, TERMINAL_DIMS]
+ENDS = np.cumsum(DIMS)
+
+##############
+# Helper Funcs
+##############
+TransRecords = namedtuple('TransRecords', 'states actions next_states rewards dones policies')
+def split_halfcheetah_v2_trans_arr(arr: str):
+    return TransRecords(*np.split(arr, ENDS, axis=1))
