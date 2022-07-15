3370892a7dd35243413a0fa7aed644a8a24b1ab9
Alan Clark <32013626+thatscotdatasci@users.noreply.github.com>
2022-07-15 18:56:14 +0100

    Updated model scoring scripts

---
+++ perform_scoring_d3rlpy_0.1M_extended_training.sh
@@ -0,0 +1,25 @@
+#! /bin/bash
+
+##################################
+# D3RLPY - RT - 0.1M Record Models
+##################################
+
+for model in \
+MP473 \
+MP474 \
+MP475 \
+MP476 \
+MP477 \
+MP478 \
+MP479 \
+MP480 \
+MP481 \
+MP482 \
+MP483 \
+MP484 \
+MP536 \
+MP537 \
+MP538
+do
+    .env/bin/python dogo/score_model_d3rlpy.py $model
+done
--- perform_scoring_mixed_3.sh
+++ perform_scoring_mixed_3.sh
@@ -16,7 +16,10 @@ MP336 \
 MP337 \
 MP338 \
 MP339 \
-MP340
+MP340 \
+MP723 \
+MP724 \
+MP725
 do
     .env/bin/python dogo/score_model_new.py $model
 done
