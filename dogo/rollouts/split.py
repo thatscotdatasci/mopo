14f288a9b9ad9fc23aefad4d658fb587dda10a1d
Alan Clark <32013626+thatscotdatasci@users.noreply.github.com>
2022-07-22 08:57:29 +0100

    Rollout splitter includes penalties

--- dogo/rollouts/split.py
+++ dogo/rollouts/split.py
@@ -11,13 +11,14 @@ ACTION_DIMS = 6
 REWARD_DIMS = 1
 TERMINAL_DIMS = 1
 POLICY_DIMS = 1
+PENALTY_DIMS = 1
 
-DIMS = [STATE_DIMS, ACTION_DIMS, STATE_DIMS, REWARD_DIMS, TERMINAL_DIMS]
+DIMS = [STATE_DIMS, ACTION_DIMS, STATE_DIMS, REWARD_DIMS, TERMINAL_DIMS, POLICY_DIMS]
 ENDS = np.cumsum(DIMS)
 
 ##############
 # Helper Funcs
 ##############
-TransRecords = namedtuple('TransRecords', 'states actions next_states rewards dones policies')
+TransRecords = namedtuple('TransRecords', 'states actions next_states rewards dones policies penalties')
 def split_halfcheetah_v2_trans_arr(arr: str):
     return TransRecords(*np.split(arr, ENDS, axis=1))
