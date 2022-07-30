6cb2a37bd937906d53ae6c727d7b9ef27239d603
Alan Clark <32013626+thatscotdatasci@users.noreply.github.com>
2022-07-30 09:38:21 +0100

    Matching medium expert parameters with paper

--- examples/config/d4rl/halfcheetah_medium_expert.py
+++ examples/config/d4rl/halfcheetah_medium_expert.py
@@ -11,7 +11,7 @@ params['kwargs'].update({
     'pool_load_path': 'd4rl/halfcheetah-medium-expert-v0',
     'pool_load_max_size': 2 * 10**6,
     'rollout_length': 5,
-    'penalty_coeff': 5.0,
+    'penalty_coeff': 1.0,
     'holdout_policy': None,
     'train_bnn_only': False,
     'rex': False,
