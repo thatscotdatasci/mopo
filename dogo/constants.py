0cb73693120d4230276b8c88a707654c5ac0528b
Alan Clark <32013626+thatscotdatasci@users.noreply.github.com>
2022-07-31 11:26:37 +0100

    Adding MOPO repo base directory to constants

--- dogo/constants.py
+++ dogo/constants.py
@@ -4,6 +4,7 @@ DEFAULT_SEED = 1443
 # Paths
 #######
 MOPO_RESULTS_MAP_PATH = "/home/ajc348/rds/hpc-work/dogo_results/mopo/results_map.json"
+MOPO_BASEDIR = "/home/ajc348/rds/hpc-work/mopo"
 RESULTS_BASEDIR = "/home/ajc348/rds/hpc-work/dogo_results/mopo"
 SCORING_BASEDIR = "/home/ajc348/rds/hpc-work/dogo_results/mopo/model_scoring"
 DATA_DIR = "/home/ajc348/rds/hpc-work/dogo_results/data"
