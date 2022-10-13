48f676ef76b1304247c0990092a522eb663b6023
Alan Clark <32013626+thatscotdatasci@users.noreply.github.com>
2022-10-13 10:42:56 +0100

    Tidy the model pool scoring scripts, including removing those no longer needed

--- dogo/model_pool/score_pool.py
+++ dogo/model_pool/score_pool.py
@@ -83,13 +83,17 @@ def score_pool(policy_experiment, env_name):
         overall_mse = np.zeros_like(rew)
         rew_mse = np.zeros_like(rew)
         for i in range(len(model_pool)):
+            # Determine the value of the unpenalised reward
             pen_rew_val = rew[i,:]
             pen_val = pen[i,:]
             unpen_rew_val = pen_rew_val + penalty_coeff*pen_val
+
             try:
                 eval_env._env.set_state(*get_qpos_qvel(obs[i,:].flatten(), env_name))
                 next_obs_real, rew_real, _, _ = eval_env.step(act[i,:])
             except MujocoException:
+                # If the MuJoCo environment throws an exception, record infinite errors
+                # This will occur when the action is degenerate
                 rew_mse[i,:] = np.inf
                 overall_mse[i,:] = np.inf
                 mujoco_exception_count += 1
@@ -99,10 +103,16 @@ def score_pool(policy_experiment, env_name):
 
                 next_obs_rew = np.hstack((next_obs[i,:], unpen_rew_val))
                 next_obs_rew_real = np.hstack((next_obs_real['observations'], rew_real))
+
+                # Rather than taking the mean MSE over the features, I take the vector norm
+                # This seems a more natural approach, but does not match what is done in the MOPO code
+                # Consequently, it does not match the approach in the model scoring scripts
                 next_obs_rew_mse = np.linalg.norm((next_obs_rew_real-next_obs_rew)**2)
                 overall_mse[i,:] = next_obs_rew_mse
 
         print(f'Encountered {mujoco_exception_count} MujocoExceptions')
+
+        # Save the results
         np.save(os.path.join(policy_exp_details.results_dir, 'models', f'mse_{os.path.basename(model_pool_path).split("_")[-1]}'), rew_mse)
         np.save(os.path.join(policy_exp_details.results_dir, 'models', f'overall_mse_{os.path.basename(model_pool_path).split("_")[-1]}'), overall_mse)
 
@@ -111,6 +121,7 @@ if __name__ == '__main__':
     policy_experiment = args.policy_experiment
     env_name = args.env_name
 
+    # Manually specify parameters when debugging
     # policy_experiment = 'HO265'
     # env_name = 'Hopper'
 
