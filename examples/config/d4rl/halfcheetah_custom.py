d17c5ae65994cea13961e7906f5565da533bb595
Alan Clark <32013626+thatscotdatasci@users.noreply.github.com>
2022-10-13 10:44:43 +0100

    Ensuring bnn_retrain_epochs parameter is in the HalfCheetah parameters file

--- examples/config/d4rl/halfcheetah_custom.py
+++ examples/config/d4rl/halfcheetah_custom.py
@@ -10,6 +10,7 @@ params.update({
 params['kwargs'].update({
     'pool_load_path': '/home/ajc348/rds/hpc-work/dogo_results/data/MIXED-RT-1.npy',
     # 'model_load_dir': '/home/ajc348/rds/hpc-work/dogo_results/mopo/ray_mopo/HalfCheetah/halfcheetah_mixed_1_101e3/seed:4321_2022-06-18_10-53-164l945x_3/models',
+    'bnn_retrain_epochs': 0,
     'pool_load_max_size': 101000,
     'rollout_length': 5,
     'rollout_batch_size': 50e3,
