79ccf6fb68c88107a7e0b6a618f20c9c72415d1b
Alan Clark <32013626+thatscotdatasci@users.noreply.github.com>
2022-10-17 15:38:06 +0100

    Adding annotations to dynamics_model_landscape

--- dogo/visualisation/dynamics_model_landscape.py
+++ dogo/visualisation/dynamics_model_landscape.py
@@ -1,31 +1,34 @@
 import argparse
 import json
 import os
-import pickle
 
 import numpy as np
 import matplotlib.pyplot as plt
 import tensorflow as tf
-from sklearn.decomposition import PCA
 
 import mopo.static
 from mopo.models.bnn import BNN
 from mopo.models.fake_env import FakeEnv
-from mopo.off_policy.loader import restore_pool_contiguous
 from softlearning.environments.utils import get_environment_from_params
-from softlearning.replay_pools.utils import get_replay_pool_from_variant
-from softlearning.replay_pools import SimpleReplayPool
-from softlearning.samplers.utils import get_sampler_from_variant
 from dogo.constants import HC_STATE_DIMS, HC_ACTION_DIMS
 from dogo.results import get_experiment_details
 from dogo.rollouts.collectors import RolloutCollector, MopoRolloutCollector
 
-
-# NOTE: Items to be aware of:
-# - the step method in FakeEnv can be run in deterministic mode, or not
-# - there are parameters in bnn_params, however these should mostly not be used
-# - choosing to be deterministic or not has a large impact
+######################################################################################
+# Explore the state, action, reward landscape defined by the learned environment model
+# (dynamics + reward) and a passed dataset of records. We can examine the penalised
+# and unpenalised rewards, the penalties themselves, and the prediction MSEs.
+######################################################################################
+# NOTE: Important items to be aware of:
+# - the `step` method in FakeEnv can be run in deterministic or stochastic mode
+# - unsurprisingly, the mode chosen has an impact on the results
+# - there are parameters in bnn_params, however these are not used
+#   o they are simply necessary to instantiate a BNN oject
 # - parameter deterministic is set to False in trining, which impacts the dynamics
+# - by default, the dynamics are run in deterministic mode
+# - running the dynamics in deterministic mode does not impact the calculation of
+#   the MOPO penalty - the standard deviations of the predictive dists are still used
+######################################################################################
 
 PARAMETERS_PATH = os.path.expanduser("~/rds/hpc-work/mopo/dogo/bnn_params.json")
 DATA_DIR = os.path.expanduser("~/rds/rds-dsk-lab-eWkDxBhxBrQ/dimorl/code/dogo_results/data")
@@ -38,17 +41,28 @@ PCA_1D = 'pca/pca_1d.pkl'
 cols = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf']
 
 def plot_reward_landscape_2d(pca_1d, transitions, metric, red_op=None, red_axis=-1):
-    fig, ax = plt.subplots(1, 1, figsize=(10,10))
+    """ Project state-action records to 1D and plot the corresponding `metric` against this.
+
+    A numpy array method should be passed to the optional `red_op` argument. This will be applied to the `metric`, over
+    the ensemble of models. For example, we might want to determine the average value the metric takes across the ensemble.
+    """
+    _, ax = plt.subplots(1, 1, figsize=(10,10))
     
+    # Extract the states and actions, and project to 1D using PCA
     obs = transitions['fake']['obs']
     acts = transitions['fake']['acts']
     obs_acts_pca = pca_1d.transform(np.hstack((obs, acts)))
     
+    # Extract the metric to be displayed
     fake_metric = transitions['fake'][metric]
+
+    # If `red_op` has been passed, apply it to the loaded values
     if red_op is not None:
         fake_metric = getattr(fake_metric, red_op)(axis=red_axis)
+
     ax.scatter(obs_acts_pca, fake_metric, marker='x', s=10, label='Model')
-    
+
+    # If the same metric has also be calculated using the real environment then plot this too    
     if metric in transitions['eval']:
         eval_metric = transitions['eval'][metric]
         if red_op is not None:
@@ -74,7 +88,7 @@ def parse_args():
 
 def get_qpos_qvel(obs):
     # Return qpos and qvel values from the current obs
-    # These are necessary if setting the environment
+    # These are necessary if setting the environment to a particular state
     qpos = np.hstack((np.zeros(1),obs[:8]))
     qvel = obs[8:]
     return qpos, qvel
@@ -133,11 +147,14 @@ def sample_transitions(args):
             act = sa_arr[i,HC_STATE_DIMS:HC_STATE_DIMS+HC_ACTION_DIMS]
             eval_env._env.set_state(*get_qpos_qvel(obs.flatten()))
         else:
-            obs = eval_env.reset()['observations']
+            raise RuntimeError('Cannot yet handle no sa_arr being passed - need to implement logic to take random action.')
+            # obs = eval_env.reset()['observations']
 
+        # Take a step in the learned environment
         next_obs, rew, _, info = fake_env.step(obs, act, deterministic=DETERMINISTIC_MODEL)
         fake_collector.add_transition(obs, act, next_obs, rew, info)
 
+        # Take a step in the real environment
         next_obs_real, rew_real, _, _ = eval_env.step(act)
         eval_collector.add_transition(obs, act, next_obs_real, rew_real)
     
@@ -156,6 +173,7 @@ def save_metric(file_prefix, transitions, metric):
     np.save(os.path.join(OUTPUT_DIR, f'{file_prefix}_{metric}.npy'), transitions['fake'][metric])
 
 def save_rewards_mse(file_prefix, transitions):
+    # Calculate and save the MSE in the reward predictions
     fake_rews = transitions['fake']['unpen_rewards']
     true_rews = transitions['eval']['rewards']
     rews_mse = ((fake_rews-true_rews)**2)
