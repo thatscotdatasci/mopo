caeaf894dd7a5e48b67adfe8b73359187dc622cd
avecplezir <avecplezir@gmail.com>
2023-02-15 12:43:52 +0400

    upd

--- examples/instrument.py
+++ examples/instrument.py
@@ -216,6 +216,7 @@ def run_example_local(example_module_name, example_argv, local_mode=False):
     example_module = importlib.import_module(example_module_name)
 
     example_args = example_module.get_parser().parse_args(example_argv)
+    print('example_args', example_args)
     variant_spec = example_module.get_variant_spec(example_args)
     trainable_class = example_module.get_trainable_class(example_args)
 
