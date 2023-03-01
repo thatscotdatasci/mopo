ae1728abe5e4391de7ca1a451e04bd6f94f70c59
avecplezir <avecplezir@gmail.com>
2023-03-01 14:22:43 +0400

    upd

--- examples/config/d4rl/walker2d_custom_rl.py
+++ examples/config/d4rl/walker2d_custom_rl.py
@@ -1,28 +1,10 @@
-import os
-from .base_mopo import mopo_params, deepcopy
+from .walker2d_custom import params, deepcopy
 
-params = deepcopy(mopo_params)
-params.update({
-    'domain': 'Walker2d',
-    'task': 'v2',
-    'exp_name': 'walker2d_mixed_rt_2',
-    'seed': 1234,
-})
+params = deepcopy(params)
 params['kwargs'].update({
-    'pool_load_path': os.path.expanduser('~/rds/rds-dsk-lab-eWkDxBhxBrQ/dimorl/code/dogo_results/data/MIXED-RT-2.npy'),
     'model_load_dir': os.path.expanduser(
-        '~/rds/rds-dsk-lab-eWkDxBhxBrQ/dimorl/code/mopo/ray_mopo/Walker2d_W-MIXED-RT-1/default/s12_2023-02-27_15-47-18bs6_l049/models'),
-    'bnn_retrain_epochs': 0,
-    'pool_load_max_size': 101000,
-    'rollout_length': 5,
-    'rollout_batch_size': 50e3,
-    'penalty_coeff': 1.0,
-    'holdout_policy': None,
-    'train_bnn_only': True,
-    'rex': False,
-    'rex_beta': 10.0,
-    'rex_multiply': True,
-    'repeat_dynamics_epochs': 1,
-    'lr_decay': 1.0,
-    'bnn_batch_size': 256
+    '~/rds/rds-dsk-lab-eWkDxBhxBrQ/dimorl/code/mopo/ray_mopo/Walker2d_W-MIXED-RT-1/default/s12_2023-02-27_15-47-18bs6_l049/models'),
+    'train_bnn_only': False,
 })
+
+
