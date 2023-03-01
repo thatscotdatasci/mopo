8acc3aa04c21a34cdd44900d36cb4aee1bf8a3c3
avecplezir <avecplezir@gmail.com>
2023-03-01 19:24:57 +0400

    upd

--- slurm_autorun/run_agent_training.py
+++ slurm_autorun/run_agent_training.py
@@ -163,6 +163,6 @@ def run_experiment_set(params_filepath):
 if __name__ == '__main__':
     # params_filepath = sys.argv[1]
 
-    params_filepath = os.path.expanduser("~/rds/rds-dsk-lab-eWkDxBhxBrQ/dimorl/code/mopo/slurm_autorun/exp_params/exp_params.rexvar.json")
+    params_filepath = os.path.expanduser("~/rds/rds-dsk-lab-eWkDxBhxBrQ/dimorl/code/mopo/slurm_autorun/exp_params/exp_params.std.json")
 
     run_experiment_set(params_filepath)
