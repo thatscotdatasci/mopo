e25d5866509762015960d4facefbe0236efd6389
avecplezir <avecplezir@gmail.com>
2023-04-26 00:46:59 +0400

    upd

--- hpc/interactive.slurm.icelake.sh
+++ hpc/interactive.slurm.icelake.sh
@@ -14,4 +14,5 @@ export TF_FORCE_GPU_ALLOW_GROWTH=true
 export RAY_DISABLE_MEMORY_MONITOR=1
 
 jupyter notebook --no-browser --ip=* --port=8081
-ssh -L 8082:cpu-q-109:8081 -fN hpc
+ssh -L 8081:cpu-q-26:8081 -fN hpc
+
