583f65f9cd9afc3245a559838fbc9b45fd66fa16
avecplezir <avecplezir@gmail.com>
2023-05-01 12:55:46 +0400

    upd

---
+++ examples/config/d4rl/hopper_mixed_rex_rewp.py
@@ -0,0 +1,12 @@
+from .hopper_mixed import params, deepcopy
+
+params = deepcopy(params)
+params['kwargs'].update({
+    'break_train_rex': False,
+    'rex': True,
+    'policy_type': 'reward_partioned',
+    'repeat_dynamics_epochs': 1,
+})
+
+
+
